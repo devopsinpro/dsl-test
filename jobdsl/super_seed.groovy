e0df33207aefa6ae26438dc96d65217be87a65ec
devopsinpro <71966632+devopsinpro@users.noreply.github.com>
2022-01-06 00:16:48 +0100

    Update super_seed.groovy

--- jobdsl/super_seed.groovy
+++ jobdsl/super_seed.groovy
@@ -1,16 +1,16 @@
 job('super-seed') {
-  scm {
-    git {
-      remote {
-        url ('git@github.com:figaw/configuration-as-code-jenkins-k8s-jobdsl.git')
-        credentials('jenkins-github-ssh')
+        scm {
+          git {
+            remote {
+              url ("git@github.com:devopsinpro/dsl-test.git")
+              credentials('jenkins-github-ssh')
+            }
+          }
+        }
+        steps {
+          dsl {
+            external('jobdsl/**/*.groovy')
+            removeAction('DELETE')
+          }
+        }
       }
-    }
-  }
-  steps {
-    dsl {
-      external('jobdsl/**/*.groovy')
-      removeAction('DELETE')
-    }
-  }
-}
