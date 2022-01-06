bb10e38a8f59094b54767aa48af574934f030d2a
devopsinpro <71966632+devopsinpro@users.noreply.github.com>
2022-01-06 13:18:38 +0100

    Update super_seed.groovy

--- jobdsl/super_seed.groovy
+++ jobdsl/super_seed.groovy
@@ -2,7 +2,7 @@ job('super-seed') {
         scm {
           git {
             remote {
-              url ("git@github.com:devopsinpro/dsl-test.git")
+              url ('git@github.com:devopsinpro/dsl-test.git')
               credentials('jenkins-github-ssh')
             }
           }
